a642fcfd6862a6d17ddc15dd7e36c59a640ed704
Gabe <42557432+spookypants@users.noreply.github.com>
2019-06-26 14:09:31 -0500

    ammending cli.js contents

--- cli.js
+++ cli.js
@@ -1,30 +1,51 @@
-var choice = process.argv[2]
-var search;
+var TV = require("./tv");
+
+var tv = new TV();
+
+
+var search = process.argv[2]
 var subject = process.argv.slice(3).join(' ');
 
-function TV (choice){
-
-    findshow();
+if (!search) {
+    search = "show";
 }
 
-function findshow(results){
-    if(choice=== 'actor'){
-        search = 'people'
-    } else if (choice === 'shows'){
-        search = 'shows';
-    }
-   $.ajax({
-       url: "http://api.tvmaze.com/search/" + search + "?q=" + subject,
-         dataType: 'json',
-         success: function(data){
-             var year = data.Year;
-             var subject = $( this ).text();
-             $(title).append(" ("+year+")");
-
-         }
-     });
-     console.log(results);
+if (!subject) {
+    subject = "Sesame Street"; 
 }
+
+if (search === "show") {
+    console.log("Searching for TV Show");
+    tv.findShow(subject);
+} else {
+    console.log("Search for TV Actor");
+    tv.findActor(subject);
+}
+
+
+// function TV (choice){
+
+//     findshow();
+// }
+
+// function findshow(results){
+//     if(choice=== 'actor'){
+//         search = 'people'
+//     } else if (choice === 'shows'){
+//         search = 'shows';
+//     }
+//    $.ajax({
+//        url: "http://api.tvmaze.com/search/" + search + "?q=" + subject,
+//          dataType: 'json',
+//          success: function(data){
+//              var year = data.Year;
+//              var subject = $( this ).text();
+//              $(title).append(" ("+year+")");
+
+//          }
+//      });
+//      console.log(results);
+// }
 
 // function findActor (){
 //     if(choice == 'Actor'){
